08c2ad2c4b56501f9de59a9fb4e0f94cbd6289be
nitin0280 <56392839+nitin0280@users.noreply.github.com>
2021-08-12 13:15:17 +0530

    Update bellman_ford.cpp

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -1,30 +1,33 @@
-vector<pair<int,int>>v[200001];
-int dist[200001];
-bool neg_cycle=false;
-int n,m;
-void bellman_ford(int source) {
-    for (int i = 0; i < n; i++) dist[i] = INT_MAX;
-    dist[source] = 0;
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n; j++) {
-            for (auto c:v[j]) {
-                if (dist[j] != INT_MAX && dist[c.f] > dist[j] + c.s) {
-                    dist[c.f] = dist[j] + c.s;
-                }
+void solve() {
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>e;
+    while(m--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        --a;--b;
+        e.push_back({a,b,-c});
+    }
+    int d[n];
+    for(int i=0;i<n;i++) d[i]=inf;
+    d[0]=0;
+    for(int i=0;i<n-1;i++){
+        for(auto &c:e){
+            if(d[c[0]]!=inf && d[c[1]]>d[c[0]]+c[2]){
+                d[c[1]]=d[c[0]]+c[2];
             }
         }
     }
-//    for(int i=0;i<n;i++)
-//    {
-//        for(auto c:v[i])
-//        {
-//            if(dist[j]!=INT_MAX && dist[c.f]>dist[j]+c.s)
-//            {
-//                neg_cycle=true;
-//                break;
-//            }
-//        }
-//    }
-
+    for(int i=0;i<n-1;i++){
+        for(auto &c:e){
+            if(d[c[0]]!=inf && d[c[1]]>d[c[0]]+c[2]){
+                d[c[1]]=ninf;
+            }
+        }
+    }
+    if(d[n-1]==ninf){
+        cout<<-1<<endl;
+    }else{
+        cout<<-d[n-1]<<endl;
+    }
 }
