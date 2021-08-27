b42a51fe57167d96eff24ac825358e2740a0d891
guptanitin280 <62015837+guptanitin280@users.noreply.github.com>
2021-08-27 22:20:29 +0530

    Update 2d-sparse-table.cpp

--- 2d-sparse-table.cpp
+++ 2d-sparse-table.cpp
@@ -60,3 +60,80 @@ public:
         );
     }
 };
+
+
+
+// log value after indices
+// const int N=1000;
+// const int M=10;
+// int tab[N+1][N+1][M+1][M+1];
+// int a[N+1][N+1];
+// int L[N+1];
+
+// struct st{
+//     int n,m;
+//     st(int _n,int _m){
+//         n=_n;
+//         m=_m;
+//         for(int i=2;i<=N;i++){
+//             L[i]=L[i/2]+1;
+//         }
+//     }
+//     int f(int x,int y,int z=0,int w=0){
+//         return max({x,y,z,w});
+//     }
+//     void build(){
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 tab[i][j][0][0]=a[i][j];
+//             }
+//         }
+//         for(int x=1;x<=M;x++){
+//             for(int i=0;i<n;i++){
+//                 for(int j=0;j<m;j++){
+//                     if(i+(1<<x)-1<n){
+//                         tab[i][j][x][0]=f(tab[i][j][x-1][0],tab[i+(1<<(x-1))][j][x-1][0]);
+//                     }
+//                 }
+//             }
+//         }
+//         for(int y=1;y<=M;y++){
+//             for(int i=0;i<n;i++){
+//                 for(int j=0;j<m;j++){
+//                     if(j+(1<<y)-1<m){
+//                         tab[i][j][0][y]=f(tab[i][j][0][y-1],tab[i][j+(1<<(y-1))][0][y-1]);
+//                     }
+//                 }
+//             }
+//         }
+//         for(int x=1;x<=M;x++){
+//             for(int y=1;y<=M;y++){
+//                 for(int i=0;i<n;i++){
+//                     for(int j=0;j<m;j++){
+//                         if(i+(1<<x)-1<n && j+(1<<y)-1<m){
+//                             tab[i][j][x][y]=f(
+//                                     tab[i][j][x-1][y-1],
+//                                     tab[i+(1<<(x-1))][j][x-1][y-1],
+//                                     tab[i][j+(1<<(y-1))][x-1][y-1],
+//                                     tab[i+(1<<(x-1))][j+(1<<(y-1))][x-1][y-1]
+//                                     );
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     // for idempotent function
+//     int qry(int x,int y,int _x,int _y){
+//         // neutral element
+//         if(_x<x || _y<y) return 0;
+//         int lx=L[_x-x+1];
+//         int ly=L[_y-y+1];
+//         return f(
+//                 tab[x][y][lx][ly],
+//                 tab[_x-(1<<lx)+1][y][lx][ly],
+//                 tab[x][_y-(1<<ly)+1][lx][ly],
+//                 tab[_x-(1<<lx)+1][_y-(1<<ly)+1][lx][ly]
+//                 );
+//     }
+// };
