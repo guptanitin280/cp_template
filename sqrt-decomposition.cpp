0f0e53af90a01f8bb4a16211c0a047b069813d59
guptanitin280 <62015837+guptanitin280@users.noreply.github.com>
2021-09-12 12:32:03 +0530

    Update sqrt-decomposition.cpp

--- sqrt-decomposition.cpp
+++ sqrt-decomposition.cpp
@@ -1,43 +1,50 @@
-class sd{
-public:
-    int n,bs,b_cnt;
-    vector<int>a,b_val,b_no,bl,br;
-    void init(int _n){
+struct sd{
+    vector<int>a,bv,bno,bl,br;
+    int n,bsz,bcnt;
 
+    sd(int _n){
         n=_n;
-        bs= sqrt(n);
-        b_cnt=(n+bs-1)/bs;
+        bsz=sqrt(n);
+        bcnt=(n+bsz-1)/bsz;
 
         a.resize(n,0);
-        b_val.resize(b_cnt,0);
-        b_no.resize(n,0);
-        bl.resize(b_cnt,-1);
-        br.resize(b_cnt,0);
-        for(int i=0;i<n;i++){
-            b_no[i]=i/bs;
-            if(bl[b_no[i]]==-1){
-                bl[b_no[i]]=i;
-            }
-            br[b_no[i]]=i;
+        bno.resize(n,0);
+
+        bl.resize(bcnt,-1);
+        br.resize(bcnt,-1);
+        bv.resize(bcnt,0);
+
+        for(int i =0;i<n;i++){
+            bno[i]=i/bsz;
+            if(bl[bno[i]]==-1) bl[bno[i]]=i;
+            br[bno[i]]=i;
         }
     }
+
     void upd(int i,int x){
-        b_val[b_no[i]]-=a[i];
+        bv[bno[i]]-=a[i];
         a[i]=x;
-        b_val[b_no[i]]+=a[i];
+        bv[bno[i]]+=a[i];
     }
+
     int qry(int l,int r){
         int tot=0;
-        if(b_no[l]==b_no[r]){
-            for(int i=l;i<=r;i++) tot+=a[i];
+        if(bno[l]==bno[r]){
+            for(int j=l;j<=r;j++){
+                tot+=a[j];
+            }
         }else{
-            for(int i=b_no[l]+1;i<b_no[r];i++)
-                tot+=b_val[i];
-            for(int i=l;i<=br[b_no[l]];i++)
-                tot+=a[i];
-            for(int i=bl[b_no[r]];i<=r;i++)
-                tot+=a[i];
+            for(int j=l;j<=br[bno[l]];j++){
+                tot+=a[j];
+            }
+            for(int j=bno[l]+1;j<bno[r];j++){
+                tot+=bv[j];
+            }
+            for(int j=bl[bno[r]];j<=r;j++){
+                tot+=a[j];
+            }
         }
         return tot;
     }
+
 };
