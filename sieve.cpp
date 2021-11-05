0ac78e187d1f260c89c17ed6e10aecd1c775b431
nitin0280 <56392839+nitin0280@users.noreply.github.com>
2021-11-05 17:42:29 +0530

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,12 +1,13 @@
+// O(nloglogn)
 const int N=1000001;
 bool prime[N];
 void sieve() {
-    mem1(prime);
-    prime[0] = prime[1] = 0;
+    for(auto &c:prime) c=true;
+    prime[0] = prime[1] = false;
     for (int i = 2; i * i < N; i++) {
         if (prime[i]) {
             for (int j = i * i; j < N; j += i) {
-                prime[j] = 0;
+                prime[j] = false;
             }
         }
     }
