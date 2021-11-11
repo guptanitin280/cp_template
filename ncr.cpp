42a6d4a96b71d45a4af1531e0f181306d9195d6a
nitin0280 <56392839+nitin0280@users.noreply.github.com>
2021-11-11 13:45:04 +0530

    Update ncr.cpp

--- ncr.cpp
+++ ncr.cpp
@@ -1,15 +1,31 @@
-int fact[300001];
-int fact_inv[300001];
+const int N=200001;
+int fact[N];
+int fact_inv[N];
 
+int power(int x, int y, int p) {
+    int res = 1;
+    x = x % p;
+    while (y > 0) {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+int modi(int a, int m) {
+    return power(a, m - 2, m);
+}
 void pre() {
     fact[0] = 1;
-    fact_inv[0] = 1;
-    for (int i = 1; i <= 300000; i++) {
+    for (int i = 1; i < N; i++) {
         fact[i] = (fact[i - 1] * i) % mod;
-        fact_inv[i] = (fact_inv[i - 1] * modi(i, mod)) % mod;
+    }
+    fact_inv[N-1]=modi(fact[N-1],mod);
+    for(int i=N-2;i>=0;i--){
+        fact_inv[i]=(fact_inv[i+1]*(i+1))%mod;
     }
 }
-
 int ncr(int n, int r) {
     if (r > n)
         return 0;
