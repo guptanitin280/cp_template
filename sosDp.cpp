c67f3d442470cbdf9d6768f237dec560681c8639
guptanitin280 <62015837+guptanitin280@users.noreply.github.com>
2022-02-08 01:19:32 +0530

    Update sosDp.cpp

--- sosDp.cpp
+++ sosDp.cpp
@@ -1,5 +1,10 @@
 // tf SOS dp
 
+
+
+
+
+
 //https://cses.fi/problemset/task/1654/
 #include <bits/stdc++.h>
 using namespace std;
@@ -10,14 +15,29 @@ const int N=(1<<20);
 int f[N]; // frequency
 int val1[N]; // number of y such that (y|x)=x;
 int val2[N]; // the number of elements y such that x&y=x
-int val3[N]; // number of y such that (y|x)=x;
+int val3[N]; // the number of elements y such that x&y≠0
 
 // sos(i,j) means from ith consider 2^j elements ( assume containing only j bits ) 
+
+// a absurd rule
+// at j=0, it is null actually because no bit, hence nothing is there so assume zero(wtf) (0|0=0), (0&0=0) or either make j=1 as base condition (best option dude)
 void sos(int i,int j) {
+    
+    // absurd if you think
     if (j == 0) {
         val1[i] = f[i], val2[i] = f[i], val3[i] = 0;
         return;
     }
+    
+    // not absurd :)
+//     if (j == 1) {
+//         val1[i] = f[i], val1[i + 1] = f[i] + f[i + 1];
+//         val2[i] = f[i] + f[i + 1], val2[i + 1] = f[i + 1];
+//         val3[i] = 0, val3[i + 1] = f[i + 1];
+//         return;
+//     }
+    
+    
     sos(i, j - 1);
     sos(i + (1 << (j - 1)), j - 1);
     int tot = 0;
