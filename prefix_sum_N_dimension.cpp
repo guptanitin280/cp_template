07cec6b19ab5015c47dd0d4bbf3328f5af992c05
guptanitin280 <62015837+guptanitin280@users.noreply.github.com>
2022-03-14 18:01:53 +0530

    Update prefix_sum_N_dimension.cpp

--- prefix_sum_N_dimension.cpp
+++ prefix_sum_N_dimension.cpp
@@ -5,7 +5,7 @@ int a[10][10];
 void method1() {
     for (int i = 1; i < 10; i++) {
         for (int j = 1; j < 10; j++) {
-            a[i][j]=a[i-1][j]+a[i][j-1]-a[i-1][j-1];
+            a[i][j]+=a[i-1][j]+a[i][j-1]-a[i-1][j-1];
         }
     }
 }
