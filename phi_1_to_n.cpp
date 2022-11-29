e8e78a1678884a345e14a5af42107107f003890c
guptanitin280 <62015837+guptanitin280@users.noreply.github.com>
2022-11-29 10:44:38 +0530

    Update phi_1_to_n.cpp

--- phi_1_to_n.cpp
+++ phi_1_to_n.cpp
@@ -1,5 +1,11 @@
+
 const int N=200001;
 int phi[N];
+
+//let p1,p2,p3....pk be the prime factors of n
+//applying inclusion and exclusion 
+//then phi[n]=n-n/p1-n/p2....-n/pk + n/(p1*p2) + n/(p2*p3) .....
+//clubbing-> phi[n]=n(1-1/p1)(1-1/p2)...(1-1/pk)
 void etf(){
     for(int i=0;i<N;i++) phi[i]=i;
     for(int i=2;i<N;i++){
@@ -9,4 +15,17 @@ void etf(){
             }
         }
     }
+}
+
+//using divisor sum property
+//eg: divisors of 10-> 1,2,5,10 -> phi[1]+phi[2]+phi[5]+phi[10]=10
+void etf(int n) {
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
 }
