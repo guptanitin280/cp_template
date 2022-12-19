0155d07dba30181236362f423020ccb1548c1f8a
guptanitin280 <62015837+guptanitin280@users.noreply.github.com>
2022-12-19 14:09:51 +0530

    Update mo's algorithm.cpp

--- mo's algorithm.cpp
+++ mo's algorithm.cpp
@@ -1,85 +1,96 @@
-struct query{
-    int i,l,r,bno;
+struct Query {
+    int i, l, r, bno;
 };
 
 //basic comparator
-bool cmp(query &a,query&b){
-    if(a.bno!=b.bno){
-        return a.bno<b.bno;
-    }else{
-        return a.r<b.r;
+bool cmp(Query &a,Query &b) {
+    if (a.bno != b.bno) {
+        return a.bno < b.bno;
+    } else if (a.bno & 1) {
+        return a.r < b.r;
+    } else {
+        return a.r > b.r;
     }
 }
 
 
-int n,q,bs;
-query qry[300001];
-int a[300001];
-int ans[300001];
+int n,q;
+const int bs=700;// change is depending on problem
+const int N=500001;
+Query qry[N];
+int a[N];
+int ans[N];
 
 
 int lp,rp,cnt;
 int freq[1000001];
 
-void add(int val){
+void add(int val) {
     freq[val]++;
-    if(freq[val]==1) cnt++;
-}
-void remove(int val){
-    freq[val]--;
-    if(freq[val]==0) cnt--;
+    if (freq[val] == 1) cnt++;
 }
 
+void remove(int val) {
+    freq[val]--;
+    if (freq[val] == 0) cnt--;
+}
+
+int get_answer() {
+    return cnt;
+}
 
 void solve() {
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-    bs=sqrt(n);
-    cin>>q;
+    cin >> q;
 
-    for(int i=0;i<q;i++){
-        int l,r;
-        cin>>l>>r;
-        --l;--r;
-        qry[i].i=i;
-        qry[i].l=l;
-        qry[i].r=r;
-        qry[i].bno=l/bs;
+    for (int i = 0; i < q; i++) {
+        int l, r;
+        cin >> l >> r;
+        --l;
+        --r;
+        qry[i].i = i;
+        qry[i].l = l;
+        qry[i].r = r;
+        qry[i].bno = l / bs;
     }
     // sort the queries
-    sort(qry,qry+q,cmp);
+    sort(qry, qry + q, cmp);
 
     // initialise the data-structure
-    lp=0;
-    rp=0;
-    cnt=0;
+    lp = 0;
+    rp = 0;
+    cnt = 0;
     add(a[0]);
 
     // answer the queries
-    for(int i=0;i<q;i++){
-        int l=qry[i].l;
-        int r=qry[i].r;
+    for (int i = 0; i < q; i++) {
+        int l = qry[i].l;
+        int r = qry[i].r;
 
-        while(rp<r){
+        while (rp < r) {
             add(a[++rp]);
         }
-        while(lp>l){
+        while (lp > l) {
             add(a[--lp]);
         }
 
-        while(rp>r){
+        while (rp > r) {
             remove(a[rp--]);
         }
-        while(lp<l){
+        while (lp < l) {
             remove(a[lp++]);
         }
 
-        ans[qry[i].i]=cnt;
+        ans[qry[i].i] = cnt;
     }
 
-    for(int i=0;i<q;i++){
-        cout<<ans[i]<<endl;
+    for (int i = 0; i < q; i++) {
+        cout << ans[i] << endl;
     }
 }
+
+// this require the element to be added on both side l,r and removed l,r
+// but we can also use mo's when data structure allow the removal from the side where we added like rollback dsu
